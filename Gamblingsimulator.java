6150c19b0f45a876abc417d6aef87f9803c6d110
“rohithgopala44” <rohithgopala44@gmail.com>
2023-02-07 14:45:09 +0530

    Added:  Gambler If Won Or Lost 50% Of The Stack, Would Resign For The Day

--- Gamblingsimulator.java
+++ Gamblingsimulator.java
@@ -9,29 +9,37 @@ public class Gamblingsimulator {
         Scanner sc = new Scanner(System.in);
         System.out.println("Starting amount of stake is "+AMOUNT_OF_STAKE);
         System.out.println("Amount to bet after every play "+BET_AMOUNT);
-        int play = (int) Math.floor(Math.random() * 10) % 2;
-        System.out.println("Gamblers play:"+play);
         int cashInHand = AMOUNT_OF_STAKE;
-        switch(play) {
-            case 1:
-                cashInHand = cashInHand + BET_AMOUNT;
-                System.out.println("Total current cash gambler has "+cashInHand);
-                break;
-            case 0:
-                cashInHand = cashInHand - BET_AMOUNT;
-                System.out.println("Total current cash gambler has "+cashInHand);
-                break;
-            default:
-                System.out.println("default");
-        }
-        if(play == 1) {
-            System.out.println("Player won the first play");
-        }
-        else {
-            System.out.println("Player lost the first play");
-        }
+        while(cashInHand > 50 && cashInHand < 150) {
+            int play = (int) Math.floor(Math.random() * 10) % 2;
+            System.out.println("Gamblers play:"+play);
 
+            switch(play) {
+                case 1:
+                    cashInHand = cashInHand + BET_AMOUNT;
+                    System.out.println("Total current cash gambler has "+cashInHand);
+                    break;
+                case 0:
+                    cashInHand = cashInHand - BET_AMOUNT;
+                    System.out.println("Total current cash gambler has "+cashInHand);
+                    break;
+                default:
+                    System.out.println("default");
+            }
+            if(play == 1) {
+                System.out.println("Player won the current play");
+            }
+            else {
+                System.out.println("Player lost the current play");
+            }
+
+        }
+        if(cashInHand==150 || cashInHand==50) {
+            System.out.println("Player would like to resign for current day");
+        }
     }
 
 }
+
+
 
