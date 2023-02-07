005bf98f44fe71f8c40ee9c09199d26a824b78d4
“rohithgopala44” <rohithgopala44@gmail.com>
2023-02-07 14:30:21 +0530

    Added: Start with 00 Stack Everyday And Bet  Every Game

---
+++ Gamblingsimulator.java
@@ -0,0 +1,13 @@
+package com.gamblingSimulator;
+
+public class Gamblingsimulator {
+    public static final int INITIAL_BET_STAKE = 100;
+    public static final int BET_AMT = 1;
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
